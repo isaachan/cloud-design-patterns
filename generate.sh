d0b7ea523a86bae229f9dba44ce85f7c506835b1
Han Kai <isaachanstar@gmail.com>
2016-10-04 16:13:55 +0800

    add script to generate pattern diagram.

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+IMG="themarquee/graphviz" 
+
+if [ ! "$(docker images | grep $IMG)" ]
+    then
+        echo "Image $IMG has not been pulled. Start pulling $IMG ..."
+	docker pull $IMG
+fi
+
+WORKING="/backup"
+rm -f patterns.png
+docker run --rm -w $WORKING -v $(pwd):$WORKING $IMG dot -Tpng patterns.dot -o patterns.png
+
